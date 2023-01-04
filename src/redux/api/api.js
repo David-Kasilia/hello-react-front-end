dea33c970949e8e00cd1bdde10ac0fbf9d87ea77
David-Kasilia <davidkasilia001@gmail.com>
2023-01-05 00:21:06 +0300

    Completed project testing

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,3 +1,6 @@
 // This is the API for the Project
+// Local host API
+// export const api = 'http://localhost:3000/api/v1/messages'
 
-export const api = 'http://localhost:3000/api/v1/messages'
+// Deployment API
+ export const api = 'https://hello-rails-backend-api.onrender.com/message/index'
